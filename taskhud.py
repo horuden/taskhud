7456f884cea2ad2138d3b5c7b94990a3c2801de4
John Ferguson <jnferguson440@gmail.com>
2017-02-13 15:43:50 -0500

    cleanup and document taskhud.py

--- taskhud.py
+++ taskhud.py
@@ -1,12 +1,31 @@
+""" ---------------------------------------------------------------------------
+
+    taskhud.py - Entry point for TaskHUD application
+
+    Copyright 2017, John Ferguson
+
+    Licensed under GPLv3, see LICENSE for full details
+
+--------------------------------------------------------------------------- """
+
 from curses import wrapper
 from cwrapper import CursesHud
 from datetime import datetime, timezone
 from taskwrapper import TaskWrapper
 
+# TODO: give this a name that better reflects its purpose
 def callback():
+    """
+    stub: called when TaskWarrior database is updated
+    """
+    # TODO: pass the new records to CursesHud
     pass
 
+# TODO: move to separate module
 def t_date(s):
+    """
+    TaskWarrior provides times as UTC timestamps in ISO 8601
+    """
     year   = int(s[0:4])
     month  = int(s[4:6])
     day    = int(s[6:8])
@@ -18,40 +37,68 @@ def t_date(s):
     # This is UTC time
     ts = datetime(year, month, day, hour, minute, second)
 
-    # This is why we can't have nice things
-    return ts.replace(tzinfo=timezone.utc).astimezone(tz=None).isoformat(sep=" ")[:-6]
+    # Convert to local time
+    local_time = ts.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    # Convert to ISO display format, and remove timezone offset
+    iso_format = local_time.isoformat(sep=" ")[:-6]
 
+    return iso_format
+
+# TODO: move to separate module
 def t_tags(s):
+    """
+    TaskWarrior provides tags as list of strings, render without "[" + "]"
+    """
     return ", ".join(s)
 
+# TODO: move to separate module
 def t_urgency(s):
+    """
+    Align urgency scores on decimal place, limit to 2 significant digits
+    """
     return "{:>6.2f}".format(s)
 
 def run_gui(screen):
+    """
+    Called by curses wrapper. Sets up HUD before running mainloop
+    """
+    # HUD object
     hud = CursesHud(screen)
 
+    # Add all tasks stored in task_wrapper at startup
+    # TODO: these should be refreshed continuously and asynchronously
     for task in task_wrapper.task_db:
         hud.add_record(task)
 
+    # These keys will be shown in bottom panel (too wide for main display)
     hud.set_extra_info("uuid")
     hud.set_extra_info("depends")
     hud.set_extra_info("annotations")
     hud.set_extra_info("modified")
 
+    # These value types need to be reformatted for display
+    ## Dates from ISO 8601 UTC -> Local time
     hud.set_translation("due", t_date)
     hud.set_translation("entry", t_date)
     hud.set_translation("end", t_date)
     hud.set_translation("start", t_date)
     hud.set_translation("scheduled", t_date)
     
+    ## Tags converted from ["x", "y", "z"] -> "x, y, z"
     hud.set_translation("tags", t_tags)
     
+    ## Align urgency scores on decimal place, 2 significant digits
     hud.set_translation("urgency", t_urgency)
 
+    # Run the HUD's main loop
     hud.mainloop()
 
 if __name__ == "__main__":
+    # Entry point
+
+    # Start up TaskWrapper and set update callback
     task_wrapper = TaskWrapper(change_cb = callback)
 
+    # Start ncurses, and pass screen object to function that sets up HUD
     wrapper(run_gui)
 
